bd4c49076c8f059054a1a218b9b3de495baacd06
Marko Vojvodic <marko.vojvodic@mongodb.com>
2015-07-22 00:39:56 -0400

    Extraction of unix timestamp and mongo message from packets

--- mongo_packet.go
+++ mongo_packet.go
@@ -10,33 +10,61 @@ import (
 // mongo wire protocol header field offset
 var OFFSET int = 4
 
+// earliest packet timestamp
+var MIN_UNIX_TIMESTAMP int64 = 0
+
+type MongoPacket struct {
+	unixTimestamp int64
+	messageLength uint32
+	requestID     uint32
+	responseTo    uint32
+	opCode        uint32
+	message       []byte
+}
+
 func process_packets(filename string) {
 	if handle, err := pcap.OpenOffline(filename); err != nil {
 		panic(err)
 	} else {
 		packetSource := gopacket.NewPacketSource(handle, handle.LinkType())
+		firstPacket := <-packetSource.Packets()
+		MIN_UNIX_TIMESTAMP = get_unix_timestamp(firstPacket)
 		for packet := range packetSource.Packets() {
 			handle_packet(packet)
 		}
 	}
 }
 
+func get_unix_timestamp(packet gopacket.Packet) int64 {
+unixTimestamp:
+	packet.Metadata().CaptureInfo.Timestamp.Unix()
+}
+
 // TODO: separate by src host/port
-// TODO: get timestamps, delta from first packet
-func handle_packet(packet gopacket.Packet) {
+func handle_packet(packet gopacket.Packet) MongoPacket {
 	// if packet contains a mongo message
 	if packet.ApplicationLayer() != nil {
 		payload := packet.ApplicationLayer().Payload()
 
+		// get timestamp's delta from first packet
+		// get mongo wire protocol payload
+		unixTimestamp := get_unix_timestamp(packet) - MIN_UNIX_TIMESTAMP
 		messageLength := binary.LittleEndian.Uint32(payload[0:OFFSET])
 		requestID := binary.LittleEndian.Uint32(payload[OFFSET : 2*OFFSET])
 		responseTo := binary.LittleEndian.Uint32(payload[2*OFFSET : 3*OFFSET])
 		opCode := binary.LittleEndian.Uint32(payload[3*OFFSET : 4*OFFSET])
+		message := payload[4*OFFSET : messageLength]
 
-		fmt.Println(messageLength)
-		fmt.Println(requestID)
-		fmt.Println(responseTo)
-		fmt.Println(opCode)
-		fmt.Println()
+		mongoPacket := MongoPacket{
+			unixTimestamp: unixTimestamp,
+			messageLength: messageLength,
+			requestID:     requestID,
+			responseTo:    responseTo,
+			opCode:        opCode,
+			message:       message,
+		}
+
+		return mongoPacket
 	}
+	return MongoPacket{unixTimestamp: packet.Metadata().CaptureInfo.Timestamp.Unix()}
 }

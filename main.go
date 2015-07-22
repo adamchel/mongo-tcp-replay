bdfc5d72cc968f8ab5ada97f554bb29f582cddad
Adam Chelminski <adam.chelminski@mongodb.com>
2015-07-22 14:09:09 -0400

    Added interactive mode optiosn

--- main.go
+++ main.go
@@ -8,8 +8,10 @@ func main() {
 	pcapFilePtr := flag.String("pcapFile", "workload.pcap", "pcap workload file")
 	mongodHostPtr := flag.String("mongodHost", "localhost", "host address for mongod")
 	mongodPortPtr := flag.String("mongodPort", "27017", "port for mongod")
+	interactiveMode := flag.Bool("interactiveMode", false, "run the pcap file in a single-threaded interactive mode.")
 
 	flag.Parse()
 
-	ProcessPackets(*pcapFilePtr, *mongodHostPtr, *mongodPortPtr)
+	ProcessPackets(*pcapFilePtr, *mongodHostPtr, *mongodPortPtr, *interactiveMode)
+
 }

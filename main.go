cd557ccbf08e4ae2f342a64c0eb365acb98336c2
Marko Vojvodic <marko.vojvodic@mongodb.com>
2015-07-22 00:01:04 -0400

    Added simple main function

--- main.go
+++ main.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Printf("Hello, world.\n")
+	argsWithoutProg := os.Args[1:]
+	if len(argsWithoutProg) == 1 {
+		process_packets(argsWithoutProg[0])
+	} else {
+		fmt.Println("please provide a *.pcap filename")
+	}
 }
